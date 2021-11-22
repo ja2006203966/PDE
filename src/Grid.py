830771d0dd40b32f42177418429cfe7a826427b3
ja2006203966 <ja2006203966@gmail.com>
2021-11-22 22:30:52 +0800

    first commit

--- src/Grid.py
+++ src/Grid.py
@@ -2,43 +2,31 @@ import numpy as np
 import copy
 from src.Space import Space
 
+def _quick(shape, a, func, n):
+    b = a.take(np.arange(shape[n]-1, shape[n]), axis=n)
+    b = np.append(a.take(np.arange(1, shape[n]), axis=n), b, axis=n)
+    delta_f = func.take(np.arange(shape[n]-1, shape[n]), axis=n)
+    delta_f = np.append(func.take(np.arange(1,shape[n]), axis=n), delta_f, axis=n)
+    return b.take(np.arange(n,n), axis = -1), copy.copy(delta_f)
+    
 class Gradient:
     def __init__(self, f, space:Space, coordination = 'Catesian'):
         self.f = f
         self.space = space
         self.coordination = coordination
         self.u, self.v, self.w = 0,0,0
+        self.dimension = space.dimension
         
     def Catesian(self,):
         a = self.space.to_numpy()
         func = self.f(a)
+        shape = a.shape
         if self.dimension >=1:
-            b = a[-1:, :, : ,:]
-            b = np.append(a[1:, :, : ,:], b, axis=0)
-            delta_f = func[-1:, :, :, :]
-            delta_f = np.append(func[1:, :, : ,:], delta_f, axis=0)
-            self.delta_b1 = b[:, :, :, 0:1]
-            self.u = copy.copy(delta_f)
-            del(delta_f)
-            del(b)
+            self.delta_b1, self.u =_quick(shape, a, func, 0)
         if self.dimension >=2:
-            b = a[:, -1:, : ,:]
-            b = np.append(a[:, 1:, : ,:], b, axis=1)
-            delta_f = func[:, -1:, : ,:]
-            delta_f = np.append(func[:, 1:, : ,:], delta_f, axis=1)
-            self.delta_b1 = b[:, :, :, 1:2]
-            self.u = copy.copy(delta_f)
-            del(delta_f)
-            del(b)
+            self.delta_b2, self.v =_quick(shape, a, func, 1)
         if self.dimension >=3:
-            b = a[:, :, -1:, :]
-            b = np.append(a[:, :, 1:, :], b, axis=2)
-            delta_f = func[:, :, -1:, :]
-            delta_f = np.append(func[:, :, 1:, :], delta_f, axis=2)
-            self.delta_b1 = b[:, :, :, 2:3]
-            self.u = copy.copy(delta_f)
-            del(delta_f)
-            del(b)
+            self.delta_b3, self.w =_quick(shape, a, func, 2)
         self.grid = self.u + self.v + self.w
-        return self.gird
+        return self.grid
         
