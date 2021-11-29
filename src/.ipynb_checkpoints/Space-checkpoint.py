a28fac8503c16ec48e5dbb3da0d31f9f91e07ba0
ja2006203966 <ja2006203966@gmail.com>
2021-11-29 20:39:33 +0800

    first commit

--- src/.ipynb_checkpoints/Space-checkpoint.py
+++ src/.ipynb_checkpoints/Space-checkpoint.py
@@ -8,12 +8,15 @@ class Space:
         self.basis = []
         if self.dimension >=1:
             self.b1 = np.linspace(*basis[0], num = bins[0])
+            self.b1_diff = (basis[0][1] - basis[0][0])/bins[0]
             self.basis.append(self.b1)
         if self.dimension >=2:
             self.b2 = np.linspace(*basis[1], num = bins[1])
+            self.b2_diff = (basis[1][1] - basis[1][0])/bins[1]
             self.basis.append(self.b2)
         if self.dimension >=3:
             self.b3 = np.linspace(*basis[2], num = bins[2])
+            self.b3_diff = (basis[2][1] - basis[2][0])/bins[2]
             self.basis.append(self.b3)
         basis_expand  = [[j for j in range(self.dimension) if j!=i] for i in range(self.dimension)]
         for i in range(self.dimension):
--- src/Grid.py
+++ src/Grid.py
@@ -2,12 +2,30 @@ import numpy as np
 import copy
 from src.Space import Space
 
-def _quick(shape, a, func, n):
-    b = a.take(np.arange(shape[n]-1, shape[n]), axis=n)
-    b = np.append(a.take(np.arange(1, shape[n]), axis=n), b, axis=n)
-    delta_f = func.take(np.arange(shape[n]-1, shape[n]), axis=n)
-    delta_f = np.append(func.take(np.arange(1,shape[n]), axis=n), delta_f, axis=n)
-    return b.take(np.arange(n,n+1), axis = -1), copy.copy(delta_f)
+def _quick(shape, a, func, n, diff):
+    a = copy.copy(a)
+    func = copy.copy(func)
+    b = a.take(np.arange(shape[n]-1, shape[n]), axis=n) + diff
+    b = np.append( a.take(np.arange(1, shape[n]), axis=n), b , axis=n)
+    b = b-a
+    b = b.take(np.arange(n, n+1), axis=-1)
+    
+    delta_f0 = func.take(np.arange(n, n+1), axis=-1) # change for (binx, biny, binz, 3) => (binx, biny, binz, 1) 
+    delta_f = delta_f0.take(np.arange(shape[n]-1, shape[n]), axis=n)
+    delta_f = np.append(delta_f0.take(np.arange(1,shape[n]), axis=n), delta_f, axis=n)
+    delta_f = delta_f -func.take(np.arange(n, n+1), axis=-1)
+    delta_f = delta_f/diff
+    
+#     delta_f = func.take(np.arange(shape[n]-1, shape[n]), axis=n)
+#     delta_f = np.append(func.take(np.arange(1,shape[n]), axis=n), delta_f, axis=n)
+# #     delta_f = delta_f.take(np.arange(n, n+1), axis=-1)
+#     delta_f = delta_f -func
+#     delta_f = delta_f/diff
+#     delta_f = delta_f.take(np.arange(n, n+1), axis=-1)
+    
+
+    return b, copy.copy(delta_f)
+
     
 class Gradient:
     def __init__(self, f, space:Space, coordination = 'Catesian'):
@@ -21,12 +39,18 @@ class Gradient:
         a = self.space.to_numpy()
         func = self.f(a)
         shape = a.shape
+        grid = []
         if self.dimension >=1:
-            self.delta_b1, self.u =_quick(shape, a, func, 0)
+            self.delta_b1, self.u =_quick(shape, a, func, 0, self.space.b1_diff)
+            grid.append(self.u)
         if self.dimension >=2:
-            self.delta_b2, self.v =_quick(shape, a, func, 1)
+            self.delta_b2, self.v =_quick(shape, a, func, 1, self.space.b2_diff)
+            grid.append(self.v)
         if self.dimension >=3:
-            self.delta_b3, self.w =_quick(shape, a, func, 2)
-        self.grid = self.u + self.v + self.w
+            self.delta_b3, self.w =_quick(shape, a, func, 2, self.space.b3_diff)
+            grid.append(self.w)
+#         self.grid = self.u + self.v + self.w
+        grid = tuple(grid)
+        self.grid = np.concatenate(grid, axis = -1)
         return self.grid
         
