a50302f40e76e86cb0e493dccc86196fc732d944
ja2006203966 <ja2006203966@gmail.com>
2021-12-03 23:02:53 +0800

    first commit

--- src/.ipynb_checkpoints/Boundary-checkpoint.py
+++ src/.ipynb_checkpoints/Boundary-checkpoint.py
@@ -6,12 +6,12 @@ def _quick(shape, a, func, n):
     al, au = a.take(np.arange(0, 1), axis=n), a.take(np.arange(shape[n]-1, shape[n]), axis=n)
     al, au = func[n][0](al), func[n][1](al)
     a = a.take(np.arange(1, shape[n]-1), axis=n)
-    a = np.concatenate((al, a, au), axis n)
+    a = np.concatenate((al, a, au), axis = n)
     return  a
 
 def _quick_num(shape, a, num, n):
     a = a.take(np.arange(1, shape[n]-1), axis=n)
-    a = np.concatenate((num[0], a, num[1]), axis n)
+    a = np.concatenate((num[0], a, num[1]), axis= n)
     return a
 
 class Boundary:
@@ -23,27 +23,26 @@ class Boundary:
         self.dimension = space.dimension
         self.bounded_space = space.to_numpy()
         self.shape = self.bounded_space.shape
-    def mapping_anlaytic():
+        self.types = types
+    def mapping_anlaytic(self,):
         if self.dimension >=1:
             self.bounded_space = _quick(self.shape, self.bounded_space, self.f, 0)
         if self.dimension >=2:
             self.bounded_space = _quick(self.shape, self.bounded_space, self.f, 1)
         if self.dimension >=3:
             self.bounded_space = _quick(self.shape, self.bounded_space, self.f, 2)
-    def mapping_numercial():
+    def mapping_numercial(self,):
         if self.dimension >=1:
             self.bounded_space = _quick_num(self.shape, self.bounded_space, self.numercial_bc, 0)
         if self.dimension >=2:
             self.bounded_space = _quick_num(self.shape, self.bounded_space, self.numercial_bc, 1)
         if self.dimension >=3:
             self.bounded_space = _quick_num(self.shape, self.bounded_space, self.numercial_bc, 2)
-            
-        
-    def get_bc():
-        if types == "Numercial":
+    def get_bc(self,):
+        if self.types == "Numercial":
             self.mapping_numercial()
             return self.bounded_space
-        if types == "Analytic":
+        if self.types == "Analytic":
             self.mapping_anlaytic()
             return self.bounded_space
         
